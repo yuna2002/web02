e993767939251b1164f1eaf1ad812d3d7416d926
yuna2002 <dladbsk674@gmail.com>
2024-09-20 09:28:23 +0900

    first commit

---
+++ src/main/java/com/example/demo/Kosta02Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kosta02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kosta02Application.class, args);
+	}
+
+}
